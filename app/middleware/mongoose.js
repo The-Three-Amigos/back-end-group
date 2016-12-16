2dbd99c75f1a11b372bd9efcb2c88b46f2141390
RpRoderick <ryanproderick@gmail.com>
2016-12-16 13:45:52 -0500

    changed mongoose
    changed middleware back to repo outline

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI || 'mongodb://jcbuzzanco:465846e@ds133348.mlab.com:33348/the-three-amigos';
+const uri = process.env.MONGODB_URI || 'mongodb://localhost/back-end-group';
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
