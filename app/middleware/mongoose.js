53ae5fc0744aed770407f17db4baf59288071aab
RpRoderick <ryanproderick@gmail.com>
2016-12-16 13:33:12 -0500

    deploy to heroku
    Replace localhost in mongoose middleware

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI || 'mongodb://localhost/back-end-group';
+const uri = process.env.MONGODB_URI || 'mongodb://jcbuzzanco:465846e@ds133348.mlab.com:33348/the-three-amigos';
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
---
+++ data/users.js
@@ -0,0 +1,10 @@
+'use strict';
+
+db.users.insert({
+      email: "ryan@",
+      password: "123",
+      surveys: {
+         question : "WhYYYYYYYYY?",
+         answers : ["Why Not?", "This is an answer too", "etc"]
+      }
+  });
