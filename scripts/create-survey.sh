1d758008f9dc13082f911b0db2b5f3b2f3324a28
jbuzzanco <jcbuzzanco@gmail.com>
2016-12-20 10:30:48 -0500

    curl request to create survey
    with all attibutes neeeded, title. question, and answers

--- scripts/create-survey.sh
+++ scripts/create-survey.sh
@@ -1,6 +1,6 @@
   #!/bin/bash
 
-    API="http://localhost:4741"
+    API="https://powerful-garden-17029.herokuapp.com"
     URL_PATH="/surveys"
 
     curl "${API}${URL_PATH}" \
@@ -9,9 +9,13 @@
       --header "Content-Type: application/json" \
       --data '{
         "survey": {
+<<<<<<< HEAD
           "title": "dogs 5",
+=======
+          "title": "dogs 3",
+>>>>>>> curl request to create survey
           "question": "what is your favorite dog?",
-          "answers": ["german shepherd"]
+          "answers": ["golden retriever"]
         }
       }'
 
